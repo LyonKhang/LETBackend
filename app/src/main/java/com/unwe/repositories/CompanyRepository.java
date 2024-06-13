8dec854e91f7ece210031222e72aac7432087653
Vinh Hunyh <vinhkhanghuynh03@gmail.com>
2024-06-13 16:08:09 -0500

    add(Company*) class comapny

---
+++ app/src/main/java/com/unwe/repositories/CompanyRepository.java
@@ -0,0 +1,11 @@
+package com.unwe.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.unwe.entities.Company;
+
+@Repository
+public interface CompanyRepository extends CrudRepository<Company, Long> {
+
+}
