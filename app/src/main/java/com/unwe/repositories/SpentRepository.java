d49653ef878c14b827fbbbdfcef3f33fd349e33f
Vinh Hunyh <vinhkhanghuynh03@gmail.com>
2024-06-14 09:57:23 -0500

    add(SQL*) adding SQL to the code. work on POST reuqest

--- app/src/main/java/com/unwe/repositories/SpentRepository.java
+++ app/src/main/java/com/unwe/repositories/SpentRepository.java
@@ -1,10 +1,11 @@
 package com.unwe.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.unwe.entities.Spent;
 
 @Repository
-public interface SpentRepository extends CrudRepository<Spent, Long> {
+public interface SpentRepository extends JpaRepository<Spent, Long> {
+
 }
